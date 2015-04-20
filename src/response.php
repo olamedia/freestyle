3b73c46d52ddb85e5030910025c7732e759a1ae4
olamedia <olamedia@gmail.com>
2015-04-21 02:29:40 +0500

    Update response.php

--- src/response.php
+++ src/response.php
@@ -13,6 +13,21 @@
 namespace freestyle;
 
 class response{
+    private static $_statusMessages = array(
+        100 => 'Continue', 101 => 'Switching Protocols',
+        200 => 'OK', 201 => 'Created', 202 => 'Accepted', 203 => 'Non-Authorative Information',
+        204 => 'No Content', 205 => 'Reset Content', 206 => 'Partial Content',
+        300 => 'Multiple Choices', 301 => 'Moved Permanently', 302 => 'Found',
+        303 => 'See Other', 304 => 'Not Modified', 305 => 'Use Proxy', 306 => 'Temporary Redirect',
+        400 => 'Bad Request', 401 => 'Unauthorized', 402 => 'Payment Required',
+        403 => 'Forbidden', 404 => 'Not Found', 405 => 'Method Not Allowed', 406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required', 408 => 'Request Timeout', 409 => 'Conflict', 410 => 'Gone',
+        411 => 'Length Required', 412 => 'Precondition Failed', 413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long', 415 => 'Unsupported Media Type', 416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        500 => 'Internal Server Error', 501 => 'Not Implemented', 502 => 'Bad Gateway',
+        503 => 'Service Unavailable', 504 => 'Gateway Timeout', 505 => 'HTTP Version Not Supported'
+        );
     private static $_callbacks = array();
     public static function on($code, $callback = null){
         if (null === $callback){
@@ -22,5 +37,39 @@ class response{
         }else{
             self::$_callbacks[$code] = $callback;
         }
+    }
+    public static function setStatus($code){
+        \header('Status: '.$code.(isset(self::$_statusMessages[$code])?self::$_statusMessages[$code]:''));
+        self::on($code);
+    }
+    public static function ok(){
+        self::setStatus(200);
+    }
+    private static function _preventLoop($location){
+        if (request::isGet() && 
+            (isset(request::$server['HTTP_REFERER']) && $location == request::$server['HTTP_REFERER'])) &&
+            $location == request::getUri()){
+            self::setStatus(500);
+        }
+    }
+    public static function movedPermanently($location){
+        self::_preventLoop($location);
+        header('Location: '.$location, true, 301);
+    }
+    public static function seeOther($location){
+        self::_preventLoop($location);
+        header('Location: '.$location, true, 303);
+    }
+    public static function notModified(){
+        self::setStatus(304);
+    }
+    public static function unauthorized(){
+        self::setStatus(401);
+    }
+    public static function forbidden(){
+        self::setStatus(403);
+    }
+    public static function notFound(){
+        self::setStatus(404);
     }
 }
